422b4d718184d14d8576168eaad6cfbd72fa52f8
Tanvir <akash01algo@gamil.com>
2024-02-10 14:44:12 +0600

    final commit

--- motivationtxtgenerator/src/App.js
+++ motivationtxtgenerator/src/App.js
@@ -1,20 +1,39 @@
-import React from "react";
+import React from 'react';
 import axios from 'axios';
-import './App.css';
-class App extends React.Component {
 
-  state = { advice: '' };
+import './App.css';
+
+class App extends React.Component {
+  state = {
+    advice: '',
+  }
 
   componentDidMount() {
-
+    this.fetchAdvice();
   }
 
   fetchAdvice = () => {
-    axios.get('https://api.adviceslip.com/advice');
+    axios.get('https://api.adviceslip.com/advice')
+      .then((response) => {
+        const { advice } = response.data.slip;
+
+        this.setState({ advice });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
+
   render() {
     return (
-      <h1>APP</h1>
+      <div className="app">
+        <div className="card">
+          <h1 className="heading">{this.state.advice}</h1>
+          <button className="button" onClick={this.fetchAdvice}>
+            <span>GIVE ME ADVICE!</span>
+          </button>
+        </div>
+      </div>
     );
   }
 }
